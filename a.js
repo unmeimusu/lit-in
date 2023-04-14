b893a2ba2d5dd422edf208511ffd99e312a9c0c4
unmeimusu <unmeimusu@gmail.com>
2023-04-15 06:21:09 +0700

    Update files

---
+++ a.js
@@ -0,0 +1,21 @@
+import {LitElement, html, css} from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js"
+
+export class Timer extends LitElement {
+    static properties = {
+        v: {}
+    }
+
+    static styles = css`...`
+
+    constructor() {
+        super()
+        this.v = "World"
+    }
+
+    render() {
+        return html`
+        <p>Hello ${this.v} page.<p>
+        `;
+    }
+}
+customElements.define('my-timer',Timer)
